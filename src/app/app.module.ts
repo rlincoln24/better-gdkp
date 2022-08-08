f8bc344f1dfd21584e9d06324cc6766f7a76f9a9
Robert Lincoln <rlincoln66@gmail.com>
2022-08-08 16:00:12 -0700

    Create Login and Navbar component
    Created the base component files for login and navbar features.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GdkpEventComponent } from './gdkp-event/gdkp-event.component';
 import { GdkpApiService } from './Shared/gdkp-api.service';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GdkpEventComponent
+    GdkpEventComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
